79be4a58830d6d7a6494906bce2f04bb54437509
Joel Norris <joel.r.norris@gmail.com>
2017-08-10 18:25:11 -0500

    initial commit, added basic files for html / js project

---
+++ README.md
@@ -0,0 +1,15 @@
+AiTN GWDB_123 Programming Logic Class
+
+Intro to JS file. 
+
+Learn how to put together a simpel vaniller JS script
+and poke at the DOM. 
+learn why u hasta put scripts at the bottom o the page
+w/ vaniller JS. 
+
+have a good one. 
+thx fer stoppin by. 
+
+peace out. 
+
+
---
+++ js/main.js
@@ -0,0 +1,13 @@
+// main.js
+///////////
+
+
+// simple dom manipulation w/ and w/out variables.
+
+document.getElementById("pageTitle").innerHTML = "Hello World";
+
+var myString = "My Awesome Project kinda sux...";
+
+var myElement = document.getElementById("pageSubTitle")
+
+myElement.innerHTML = myString;
