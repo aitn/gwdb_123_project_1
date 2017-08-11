de5faf40dac96322a21ee8db5957e352449a07e5
Joel Norris <joel.r.norris@gmail.com>
2017-08-10 19:10:37 -0500

    lernt about console loggin

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ var myString = "My Awesome Project kinda sux...";
 var myElement = document.getElementById("pageSubTitle")
 
 myElement.innerHTML = myString;
+
+
+// console loggin'
+
+var x;
+var y = 11;
+
+console.log(myString);
+console.log(myElement);
+console.log(x);
+console.log(y);
+
+// 
