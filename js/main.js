a0695c26035b822da6da1bd57f9e40bfa148872c
Student <student@2041-01-2105-22.local>
2017-08-24 18:43:40 -0500

    added additional console logging statements

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
-// main.js
-///////////
-
-
-// simple dom manipulation w/ and w/out variables.
+// JavaScript Document
 
 document.getElementById("pageTitle").innerHTML = "Hello World";
 
-var myString = "My Awesome Project kinda sux...";
+var myString = "My awesome project kinda sucks...";
 
-var myElement = document.getElementById("pageSubTitle")
+var myElement = document.getElementById("pageSubTitle");
 
 myElement.innerHTML = myString;
 
-
 // console loggin'
 
+var w = 42;
 var x;
-var y = 11;
 
-console.log(myString);
+var y = 42;
+var z = "42";
+
+var answer = w + y;
+var answer2 = y + z;
+
+
+console.info("Now we're loggin'");
 console.log(myElement);
+console.log(myString);
 console.log(x);
 console.log(y);
+console.log("-------");
+console.log(answer);
+console.log(answer2);
 
-// 
