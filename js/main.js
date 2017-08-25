c34cec916e35da61dbb313a3d0dcd9f31409c64f
Student <student@2041-01-2105-22.local>
2017-08-24 19:51:53 -0500

    added empty stylesheet and anonymous function binding

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // JavaScript Document
 
+
+// simple dom manipulation
+
 document.getElementById("pageTitle").innerHTML = "Hello World";
 
 var myString = "My awesome project kinda sucks...";
@@ -16,8 +19,9 @@ var x;
 var y = 42;
 var z = "42";
 
-var answer = w + y;
-var answer2 = y + z;
+// fun w/ auto~typing.
+var answer = w + y; // num + num
+var answer2 = y + z; // num + string lol.
 
 
 console.info("Now we're loggin'");
@@ -29,3 +33,25 @@ console.log("-------");
 console.log(answer);
 console.log(answer2);
 
+// click events and anonymous functions
+
+var myButtons = document.getElementsByClassName("button");
+var userMessage = document.getElementById("userMessage");
+
+for (var i=0; i<myButtons.length; i++) {
+	
+	myButtons[i].onclick = function(e) {
+		
+		e.preventDefault();
+		console.log(e);
+		// you should never use javascript alerts. it is no longer the 90's...
+		// alert("the " + e.srcElement.localName + " " + e.srcElement.className + " was clicked!!!");
+		
+		// create a message for the user and add it to the DOM
+		var myMessage = "the " + e.srcElement.localName + " " + e.srcElement.className + " was clicked!!!";
+		userMessage.innerHTML = myMessage;
+		
+	}
+	
+	
+}
